f500de9571124bcf43db269af3b98cb213636254
nctimmer <60162672+nctimmer@users.noreply.github.com>
2020-09-21 22:27:25 -0400

    trying to understand the instructions and implement it into code

--- Assignments/assignment4.py
+++ Assignments/assignment4.py
@@ -1,3 +1,9 @@
 #Assignment 4
 #Noah Timmer
 
+lower = int(input("Enter the smallest number in the range: "))
+upper = int(input("Enter the biggest number in the range:  "))
+sum = 0
+
+for n in range(lower, upper +1 ):
+
