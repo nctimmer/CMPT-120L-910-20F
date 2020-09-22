0c2d4ef06ce54b7f7de5c29ed9e965d161eb5f77
nctimmer <60162672+nctimmer@users.noreply.github.com>
2020-09-21 22:34:09 -0400

    putting the function within a class in order for function 2 to call upon function 1

--- Assignments/assignment4.py
+++ Assignments/assignment4.py
@@ -1,10 +1,19 @@
 #Assignment 4
 #Noah Timmer
 
-lower = int(input("Enter the smallest number in the range: "))
-upper = int(input("Enter the biggest number in the range:  "))
-sum = 0
+class Main:
+
+    def Function1(self):
+        lower = int(input("Enter the smallest number in the range: "))
+        upper = int(input("Enter the biggest number in the range:  "))
+        sum = 0
+
+
+
 
 for n in range(lower, upper +1 ):
     sum = sum + n
+    n = n + 1
+
+print("The sum is: ", sum)
 
