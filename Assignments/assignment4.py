902773ae2330faf0c0d89a261f84aef73f902ffc
nctimmer <60162672+nctimmer@users.noreply.github.com>
2020-09-21 22:44:04 -0400

    at a standstill, have no idea what I'm doing

--- Assignments/assignment4.py
+++ Assignments/assignment4.py
@@ -1,16 +1,11 @@
 #Assignment 4
 #Noah Timmer
 
-class Main:
-
-    def Function1(self):
-        lower = int(input("Enter the smallest number in the range: "))
-        upper = int(input("Enter the biggest number in the range:  "))
-        sum = 0
-
-
-
-
+ def Function1(self):
+     lower = int(input("Enter the smallest number in the range: "))
+     upper = int(input("Enter the biggest number in the range:  "))
+     sum = 0
+     
 for n in range(lower, upper +1 ):
     sum = sum + n
     n = n + 1
