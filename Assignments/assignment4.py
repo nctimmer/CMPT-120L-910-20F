78241e491a280017364babddc4a699dacae9c8b8
nctimmer <60162672+nctimmer@users.noreply.github.com>
2020-09-26 13:37:22 -0400

    trying to figure out how to create this range

--- Assignments/assignment4.py
+++ Assignments/assignment4.py
@@ -1,15 +1,6 @@
 #Assignment 4
 #Noah Timmer
 
- def Function1(self):
-     lower = int(input("Enter the smallest number in the range: "))
-     upper = int(input("Enter the biggest number in the range:  "))
-     sum = 0
-     
-for n in range(lower, upper +1 ):
-    sum = sum + n
-    n = n + 1
-
-print("The sum is: ", sum)
+end = input("Enter an end value for the range:")
 
 #NEED A LOOP
