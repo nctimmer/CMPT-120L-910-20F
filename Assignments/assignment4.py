0ef65392281d8f8cba36a611744a97fa62a44ca6
nctimmer <60162672+nctimmer@users.noreply.github.com>
2020-09-26 13:51:01 -0400

    creating for loop

--- Assignments/assignment4.py
+++ Assignments/assignment4.py
@@ -4,4 +4,9 @@
 lower = int(input("Enter the lower number of the range (1):"))
 upper = int(input("Enter the upper number of the range:"))
 
+sum = 0
+
+for x in range(lower, upper + 1):
+    sum = sum + x
+
 #NEED A LOOP
