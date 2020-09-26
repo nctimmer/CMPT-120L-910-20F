115bdc72e2b7003b72ae2144d0cf56435e9a581e
nctimmer <60162672+nctimmer@users.noreply.github.com>
2020-09-26 13:56:14 -0400

    changed variables for function1() and added spaces after the input statements

--- Assignments/assignment4.py
+++ Assignments/assignment4.py
@@ -1,12 +1,16 @@
 #Assignment 4
 #Noah Timmer
 
-lower = int(input("Enter the lower number of the range (1):"))
-upper = int(input("Enter the upper number of the range:"))
+def function1():
+min = int(input("Enter the lower number of the range (1): "))
+max = int(input("Enter the upper number of the range: "))
 
 sum = 0
 
-for x in range(lower, upper + 1):
+for x in range(min, max + 1):
     sum = sum + x
 
-#NEED A LOOP
+print("Sum:", sum)
+
+def main():
+    second = int(input("Enter a random number: "))
