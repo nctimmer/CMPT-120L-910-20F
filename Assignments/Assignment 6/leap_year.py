283c278043ed40670bff69b66f084520e3fc1c9f
nctimmer <60162672+nctimmer@users.noreply.github.com>
2020-10-27 00:02:04 -0400

    added comma after year in print function

--- Assignments/Assignment 6/leap_year.py
+++ Assignments/Assignment 6/leap_year.py
@@ -3,7 +3,9 @@ def leap_year(year):
     year = int(input("Enter the year:" ))
 
     if year % 4 == 0 and year % 100 != 0:
-        print(year "is a Leap Year!")
+        print(year, "is a Leap Year!")
+    elif year % 100 == 0:
+
 
 if __name__ == "__main__":
     years = [2000, 1994, 1912, 3002, 1700, 1400]
