c9408cef5aabd78dd61264c364a43b9f5c6b5d9e
nctimmer <60162672+nctimmer@users.noreply.github.com>
2020-10-27 00:03:25 -0400

    created all if, elif, and else functions

--- Assignments/Assignment 6/leap_year.py
+++ Assignments/Assignment 6/leap_year.py
@@ -5,6 +5,11 @@ def leap_year(year):
     if year % 4 == 0 and year % 100 != 0:
         print(year, "is a Leap Year!")
     elif year % 100 == 0:
+        print(year, "is not a Leap Year.")
+    elif year % 400 == 0:
+        print(year, "is a Leap Year!")
+    else:
+        print(year, "is not a Leap Year")
 
 
 if __name__ == "__main__":
