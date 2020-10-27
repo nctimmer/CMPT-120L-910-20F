b5a4d14cd905e4c3e9d387e303943ae79247155b
nctimmer <60162672+nctimmer@users.noreply.github.com>
2020-10-27 00:01:24 -0400

    first if statement, combining 2 possible year scenarios

--- Assignments/Assignment 6/leap_year.py
+++ Assignments/Assignment 6/leap_year.py
@@ -2,6 +2,9 @@ def leap_year(year):
     # Write your code here. 
     year = int(input("Enter the year:" ))
 
+    if year % 4 == 0 and year % 100 != 0:
+        print(year "is a Leap Year!")
+
 if __name__ == "__main__":
     years = [2000, 1994, 1912, 3002, 1700, 1400]
     answers = []
