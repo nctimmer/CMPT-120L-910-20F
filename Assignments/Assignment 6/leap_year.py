017e983e99079fa065cdb6c7219b68e3fce28889
nctimmer <60162672+nctimmer@users.noreply.github.com>
2020-10-27 00:33:53 -0400

    added modulo

--- Assignments/Assignment 6/leap_year.py
+++ Assignments/Assignment 6/leap_year.py
@@ -2,7 +2,7 @@ def leap_year(year):
     # Write your code here. 
     if (year % 4) == 0:
         if (year % 100) == 0:
-            if (year 400) == 0:
+            if (year % 400) == 0:
                 print(year, "is a Leap Year!")
             else:
                 print(year, "is not a Leap Year.")
