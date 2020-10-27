24ece32085a96c523ff153233dd280cbf4b4e360
nctimmer <60162672+nctimmer@users.noreply.github.com>
2020-10-27 00:20:23 -0400

    deleted repetitive elif command with no change to the output

--- Assignments/Assignment 6/leap_year.py
+++ Assignments/Assignment 6/leap_year.py
@@ -2,8 +2,6 @@ def leap_year(year):
     # Write your code here. 
     if year % 4 == 0 and year % 100 == 0:
         print(year, "is a Leap Year!")
-    elif year % 100 == 0:
-        print(year, "is not a Leap Year.")
     elif year % 400 == 0:
         print(year, "is a Leap Year!")
     else:
