52d912986143ce446a507f7d53777cf9387997f2
nctimmer <60162672+nctimmer@users.noreply.github.com>
2020-11-05 13:13:59 -0500

    added the two extreme values, program didn't load the positive extreme

--- Assignments/Assignment 7/prime_or_composite.py
+++ Assignments/Assignment 7/prime_or_composite.py
@@ -7,7 +7,7 @@ def prime_or_composite(number):
     - Take in a parameter called number and return “Prime” or “Composite”
     """
     if number > 1:
-        for i in range(2, number):
+        for i in range(2,number):
             if(number % i) == 0:
                 print(number, "is Composite")
         else:
@@ -16,11 +16,11 @@ def prime_or_composite(number):
         print(number, "is Composite")
 
 if __name__ == "__main__":
-    numbers = [1, 2, 10, 31, 47, 89, 101, 103, 97, 187, 981, 19201]
+    numbers = [-7, 1, 2, 10, 31, 47, 89, 101, 103, 97, 187, 981, 19201, 47055833459]
     # If you want to test the efficency of your algorithm add this number to the array above -7
     # If you want to test the efficency of your algorithm add this number to the array above 47055833459
     answers = []
     for number in numbers:
         answers.append(prime_or_composite(number))
-    
+
     print(answers)
