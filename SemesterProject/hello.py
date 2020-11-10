0b16e811ec6156c0770beffd292a036c8a880c53
nctimmer <60162672+nctimmer@users.noreply.github.com>
2020-11-09 23:10:41 -0500

    created Lab 7jpynb but have no idea how to make it look like something we've done before

--- SemesterProject/hello.py
+++ SemesterProject/hello.py
@@ -1,13 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-#don't think "app" object was actually created
+"""
 def hello_world():
     return 'Hello, World!'
-
-from flask import render_template
-
+"""
 @app.route('/hello/')
 @app.route('/hello/<name>')
 def hello(name=None):
