fede723c360af930c99c38b544ae9926f70c7822
nctimmer <60162672+nctimmer@users.noreply.github.com>
2020-11-10 21:39:52 -0500

    made changes according to what was presented in class by Kippins

--- SemesterProject/hello.py
+++ SemesterProject/hello.py
@@ -2,11 +2,9 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-"""
-def hello_world():
-    return 'Hello, World!'
-"""
+def index():
+    return render_template("index.html")
+
 @app.route('/hello/')
-@app.route('/hello/<name>')
-def hello(name=None):
-    return render_template('hello.html', name=name)
+def hello_world():
+    return "Hello, ME!"
