e2fb760a57214cd1cb029ee3a2547ed3174de739
nctimmer <60162672+nctimmer@users.noreply.github.com>
2020-11-23 12:55:07 -0500

    imported logging lines from lesson 10

--- assignment10.py
+++ assignment10.py
@@ -1,4 +1,13 @@
 import argparse
 parser = argparse.ArgumentParser()
-
 parser.add_argument("-r", "--reverse", help="This will reverse the order of the array", action="store_true")
+
+import logging
+logger = logging.getLogger()
+logger.setLevel(logging.WARNING)
+
+logging.info("This is an info log message")
+logging.warning("This is an warning log message")
+logging.debug("This is an debug log message")
+logging.error("This is an error log message")
+logging.critical("This is an critical log message")
