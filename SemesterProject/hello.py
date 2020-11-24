09921bbb8dccceda59769b5db8bffe295d1324cb
nctimmer <60162672+nctimmer@users.noreply.github.com>
2020-11-24 16:01:21 -0500

    added the logger.debug statements to my two app.routes

--- SemesterProject/hello.py
+++ SemesterProject/hello.py
@@ -6,7 +6,13 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template("index.html")
+app.logger.debug('400')
+app.logger.warning('A warning occurred (%d apples)', 42)
+app.logger.error('An error occurred')
 
 @app.route('/hello/')
 def hello_world():
     return "Hello, ME!"
+app.logger.debug('400')
+app.logger.warning('A warning occurred (%d apples)', 42)
+app.logger.error('An error occurred')
